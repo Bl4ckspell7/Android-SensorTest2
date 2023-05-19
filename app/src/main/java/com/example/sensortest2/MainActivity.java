ed1d1dc86cfabbd83d2cc7143bf36c01c5bb849f
Bl4ckspell7 <robertheyd@proton.me>
2023-05-19 21:40:35 +0200

    implemented tab3 logic and ui

--- app/src/main/java/com/example/sensortest2/MainActivity.java
+++ app/src/main/java/com/example/sensortest2/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.sensortest2;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.os.VibratorManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -11,9 +18,25 @@ import androidx.fragment.app.FragmentTransaction;
 import com.example.sensortest2.databinding.ActivityMainBinding;
 
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    SensorManager sensorManager;
+    Sensor magneticFieldSensor;
+    Sensor accelerometerSensor;
+    Sensor proximitySensor;
+    Sensor lightSensor;
     ActivityMainBinding binding;
+    VibratorManager vibratorManager;
+    static Vibrator vibrator;
+    final static int VIBRATION_DURATION = 120;
+    private final float[] accelerometerReading = new float[3];
+    private final float[] magnetometerReading = new float[3];
+    private final float[] rotationMatrix = new float[9];
+    private final float[] orientationAngles = new float[3];
+    public static float val0;
+    public static int val1, val2;
+    public static double proximitySensorValue;
+    public static float lightSensorValue;
+    private FragmentRefreshListener fragmentRefreshListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +44,7 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         replaceFragment(new Tab1Fragment());
-
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
-
             int itemId = item.getItemId();
             if (itemId == R.id.tab1) {
                 replaceFragment(new Tab1Fragment());
@@ -34,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
             }
             return true;
         });
+        attachSensors();
     }
 
     private void replaceFragment(Fragment fragment) {
@@ -43,8 +65,93 @@ public class MainActivity extends AppCompatActivity {
         fragmentTransaction.commit();
     }
 
-    private void toast(String string) {
-        Toast toast = Toast.makeText(getApplicationContext(), string, Toast.LENGTH_SHORT);
-        toast.show();
+    private void attachSensors() {
+        vibratorManager = (VibratorManager) getSystemService(Context.VIBRATOR_MANAGER_SERVICE);
+        vibrator = vibratorManager.getDefaultVibrator();
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        magneticFieldSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+    }
+
+    public static void vibrate() {
+        vibrator.vibrate(VibrationEffect.createOneShot(VIBRATION_DURATION, VibrationEffect.DEFAULT_AMPLITUDE));
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            System.arraycopy(sensorEvent.values, 0, accelerometerReading, 0, accelerometerReading.length);
+        }
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            System.arraycopy(sensorEvent.values, 0, magnetometerReading, 0, magnetometerReading.length);
+        }
+
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_PROXIMITY) {
+            proximitySensorValue = sensorEvent.values[0];
+        }
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_LIGHT) {
+            lightSensorValue = sensorEvent.values[0];
+        }
+        updateOrientationAngles();
+        set_val();
+        refreshFragment();
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (accelerometerSensor != null)
+            sensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI);
+        if (magneticFieldSensor != null)
+            sensorManager.registerListener(this, magneticFieldSensor, SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI);
+        if (proximitySensor != null)
+            sensorManager.registerListener(this, proximitySensor, SensorManager.SENSOR_DELAY_NORMAL);
+        if (lightSensor != null)
+            sensorManager.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    public void updateOrientationAngles() {
+        SensorManager.getRotationMatrix(rotationMatrix, null, accelerometerReading, magnetometerReading);
+        SensorManager.getOrientation(rotationMatrix, orientationAngles);
+    }
+
+    private void set_val() {
+        val0 = (float) (-orientationAngles[0] * 180 / 3.1415926535897); //Bogen in Gradmaß
+        val1 = (int) (orientationAngles[1] * 180 / 3.1415926535897);
+        val2 = (int) (orientationAngles[2] * 180 / 3.1415926535897);
+    }
+
+    public interface FragmentRefreshListener {
+        void refresh();
+    }
+
+    public void setFragmentRefreshListener(FragmentRefreshListener fragmentRefreshListener) {
+        this.fragmentRefreshListener = fragmentRefreshListener;
+    }
+
+    public FragmentRefreshListener getFragmentRefreshListener() {
+        return fragmentRefreshListener;
+    }
+
+    private void refreshFragment() {
+        if (getFragmentRefreshListener() != null) {
+            getFragmentRefreshListener().refresh();
+        }
     }
 }
